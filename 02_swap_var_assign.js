cd8955df3bb1ead7a29fe5980e7f500d57c4bcbd
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-06 22:24:03 +0530

    2nd assignment
    2nd assignment

---
+++ 02_swap_var_assign.js
@@ -0,0 +1,14 @@
+var sweety; 
+var cutie;
+
+console.log("Before swaping");
+
+console.log("Sweety,cutie");
+console.log("After swapping");
+console.log("cutie,Sweety");
+console.log("Before swapping");
+var num1 = "100",num2 = "200",num3 ="300"
+console.log(num1,num2,num3);
+console.log("After swapping");
+var num2 = "200",num3 = "300",num1 = "100";
+console.log(num2,num3,num1);
