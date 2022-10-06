324d2aec01ff3a825a961fcd0949682a58ed46fb
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-06 07:00:51 +0530

    update
    update

---
+++ demo.js
@@ -0,0 +1,6 @@
+//  This is my Program
+// Now i know the two way to rum program
+console.log{ Hello};
+console.log{"My name Shubham Kandelkar"}
+console.info{"This is safe zone"}
+console.info{"I not not bad boys"}
