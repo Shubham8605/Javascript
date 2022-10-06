363ab2c94f4c2025dccec72ff4bc5dbdc951ba43
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-06 21:47:36 +0530

    variable assignment
    variable assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+var my_full_name
+my_full_name = "Shubham Kandelkar";
+console.log(my_full_name);
+
+my_full_name = "Shubham Sanjay Kandelkar";
+console.log(my_full_name);
+
+var my_love = "i love only JS";
+console.log(my_love);
+
+var firstname = "Shubham";
+console.log("firstname:",firstname);
+var lastname = "Kandelkar";
+console.log("Lastname:",lastname);
+var age = "24";
+console.log("age:",age);
+var address = "At Post Datala";
+console.log("address:",address);
+var pincode = "443102";
+console.log("pincode:",pincode);
+var gender = "male";
+console.log("gender:", gender);
+var marritialstatus ="Unmarried";
+console.log("marritialstatus:" , marritialstatus);
