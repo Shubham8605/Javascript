f7a28ae2ee0dc03ee450cf475a6f205a15af011a
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-06 08:45:27 +0530

    commit
    commit

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var my_apple; // variable declaration
+my_apple = "Strawberry"; // variable initialization
+
+//var my_name = "Shubham Kandelkar" // variable declaration initilization on same line 
+//my_name = "Shubham Sanjay Kandelkar"
+//console.log(my_name);
+console.log(my_apple);
+
+my_apple = "200";
+console.log(my_apple);
