9d782f57155a2db9cf3ea8bdc843ccbca923f8eb
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-12 22:23:58 +0530

    string
    string

--- 04_string_assignment_02.js
+++ 04_string_assignment_02.js
@@ -9,9 +9,36 @@ console.log(count);
 
 console.log(string.length);
 
-console.log("=====Q3=====");
- var sonali = "remove the leading amd trailing extra space";
+console.log("====Q3====");
+var ajay = "remove the leading amd trailing extra space:";
+ console.log(ajay);
+ var sama = "Hey you are doing good,keep it up";
+ console.log(sama);
+
+
+
+console.log("=====Q4=====");
+ var sonali = "yotal number of extra spaces count that is rremoved";
  console.log(sonali);
 
+ var trimResult = string.trim();
+ console.log(trimResult.length );
 
- 
+console.log("=====Q5=====");
+
+var fchar = "first character after trim : H";
+console.log(fchar);
+var lchar = "last character after trim : P";
+console.log(lchar);
+
+
+console.log("=====Q6======");
+console.log("******split*******");
+var string = "Hey you are doing good ,keep it up  ";
+var splitResult = string.split(`,`);
+console.log(splitResult);
+
+console.log("====Q7=====");
+var index = "Index of word good form the given string:";
+console.log(index);
+var char = string.indexOf(0);
