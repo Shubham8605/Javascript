f5e1a1562771cf5654a0c63c06e4b40ebc6b753e
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-12 21:45:39 +0530

    update
    update

---
+++ 04_string_assignment.js
@@ -0,0 +1,13 @@
+console.log("=====My Dream company name======= ");
+
+var string = "TCS";
+console.log(string);
+
+ console.log("=====My Hobbies======");
+
+ var Hobbies1 = "Playing Cricket";
+ console.log(Hobbies1);
+ var Hobbies2 = "Wachting Movies";
+ console.log(Hobbies2);
+ var Hobbies3 = "Listening music";
+ console.log(Hobbies3);
---
+++ 04_string_assignment_02.js
@@ -0,0 +1,17 @@
+console.log("======Q1========");
+
+var string = "   Hey you are doing good ,keep it up  ";
+console.log(string);
+
+console.log("=====Q2======");
+var count = "calculate the length of string";
+console.log(count);
+
+console.log(string.length);
+
+console.log("=====Q3=====");
+ var sonali = "remove the leading amd trailing extra space";
+ console.log(sonali);
+
+
+ 
