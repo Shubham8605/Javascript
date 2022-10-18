5bfc8aac7bf0b9de9291789b7c975c47dbcae8c8
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-18 08:32:02 +0530

    while,doWhileloop,scope
    while,doWhileloop,scope

---
+++ 06_dowhile_loop.js
@@ -0,0 +1,19 @@
+var count = 0;
+do {
+    console.log(count);
+    count++;
+} while (count<=5);
+
+var count = 80;
+do {
+    console.log(count);
+    count--;
+} while (count>70);
+
+var square = 1;
+var addition = 1;
+for (let index = 1; index <=5; index++) {
+    square = index*index;
+    addition = addition + square;
+    
+}
---
+++ 06_while_loop.js
@@ -0,0 +1,19 @@
+var count = 0;
+
+while (count<=5) {
+    console.log(count);
+    count++;
+}
+var count = 10;
+
+while (count>=0) {
+    console.log(count);
+    count--;
+}
+
+var count = 5;
+while (count<=50) {
+    console.log(count);
+    count = count+5;
+
+}
