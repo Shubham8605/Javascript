4978c695c69f4edbd83b4ced81b7465e1abf1d0e
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-21 08:37:22 +0530

    date
    date

--- 09_class.js
+++ 09_class.js
@@ -12,6 +12,10 @@ details(){
 }
 let sachin = new Person("Sacin kale","pune","45","male");
 let Rohit = new Person("Rohit sharma","mumbai","32","male");
+
+sachin.fullName;
+sachin.details();
+
 console.log(sachin);
 console.log(Rohit);
 
