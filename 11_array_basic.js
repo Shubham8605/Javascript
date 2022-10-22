b69a9a335c5b03ebf36945a50e1e4e9c3bc7a81f
Shubham8605 <113613048+Shubham8605@users.noreply.github.com>
2022-10-22 08:47:25 +0530

    arrayBasic
    arrayBasic

---
+++ 11_array_basic.js
@@ -0,0 +1,6 @@
+const array1 = [1, 3, 4];
+const array2 = [5, 6, 7];
+// const array3 = array1.concat(array2);
+const array3 = [...array1, ...array2]
+console.log(array3);
+console.log("-----------------------------");
